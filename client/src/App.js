b642ef652c85b78bcd946b87a518cb6fdafe2d81
Samyakkumar <samyakkumar@gmail.com>
2019-02-10 19:52:22 -0500

    Fix App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,22 @@ import BFSNav from './components/Navbar';
 class App extends Component {
   render() {
     return (
-      <BFSNav />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
