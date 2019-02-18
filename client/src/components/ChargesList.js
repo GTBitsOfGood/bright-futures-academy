a3824e94ee00b733bf731a9dc37e39286f7376e7
Michelle Liu <michelleliu343@gmail.com>
2019-02-18 12:05:26 -0500

    Add descriptive comments and delete excess

--- client/src/components/ChargesList.js
+++ client/src/components/ChargesList.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './PaymentContent.css';
 
 /**
- * Content for page 0.
+ * List of charges to student account, names + prices retrieved from backend
  */
-class PaymentContent0 extends Component {
+class ChargesList extends Component {
   constructor(props) {
     super(props);
 
@@ -37,4 +37,4 @@ class PaymentContent0 extends Component {
 
 
 
-export default PaymentContent0;
+export default ChargesList;
