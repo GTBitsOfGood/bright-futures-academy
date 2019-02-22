2a9cf87ece0b52f0f1d1d321a0d4b26bf5c2b652
Samyakkumar <samyakkumar@gmail.com>
2019-02-21 20:27:27 -0500

    Add router

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NoMatch extends Component {
+    render() {
+        return (
+            <div>
+                <h1>404: Route not found</h1>
+            </div>
+        )
+    }
+}
+
+export default NoMatch;
