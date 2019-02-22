bfe53262a8563b0ea3798ab84b06621552506d22
Philippe Desrosiers <phildesro@yahoo.com>
2019-02-22 15:57:23 -0500

    Fixed conflict

--- server/testServer.js
+++ server/testServer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config()
-
+const mongoose = require('mongoose')
 const app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,7 +9,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(() => {
+    console.log("Connected to MongoDB")
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
