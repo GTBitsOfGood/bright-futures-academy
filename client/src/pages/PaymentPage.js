4f4be292401c20ac4b70d1e1d4cf3516dde41b4b
spencerhyphengold <spencersnotgold@gmail.com>
2019-02-25 14:25:30 -0500

    Pass down onClickNextPage as props (#47)
    * Pass down onClickNextPage as props to update state
    
    * Revert to master's package-lock
    
    * Add TODO for Model.js

--- client/src/pages/PaymentPage.js
+++ client/src/pages/PaymentPage.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
+// TODO: make an index.js that exports all of these
 import PaymentPageContent from '../components/PaymentPageContent';
-import PaymentPageProgress from '../components/PaymentPageProgress'; // TODO: make a class that will export all these
+import PaymentPageProgress from '../components/PaymentPageProgress'; 
+
+// TODO: get this value from new Model.js file described below
+const NUM_PAGES = 4;
 
 /**
  * Parent class for Payment Page. Contains the text "Account Payment", then the progress bar through the pages, and a
@@ -11,23 +15,34 @@ class PaymentPage extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
         currentPage: 1, // will be used to determine what page user is on. We start at page 1, end on numPages
-        numPages: 4,
-        activities: [{name:"Activity Fee", price:"400"}, {name:"Uniform Fee", price:"40"}, {name:"Transportation Fee", price:"40"}, {name:"Textbook Fee", price:"40"}]
+        activities: [],
     }
+  }
 
+  componentDidMount() {
+    // activities is a placeholder, to be replaced with an API call
+    const activities = [{name:"Activity Fee", price:"400"}, {name:"Uniform Fee", price:"40"}, {name:"Transportation Fee", price:"40"}, {name:"Textbook Fee", price:"40"}];
+    this.setState({ activities: activities });
   }
 
   render() {
+    const { activities, currentPage } = this.state;
     return (
       <div>
         <h2>Account Payment</h2>
-        <PaymentPageProgress currentPage={this.state.currentPage} numPages={this.state.numPages}/>
-        <PaymentPageContent currentPage={this.state.currentPage} activities={this.state.activities}/>
+        <PaymentPageProgress currentPage={currentPage} numPages={NUM_PAGES} />
+        <PaymentPageContent currentPage={currentPage} activities={activities} onClickNextPage={this.onClickNextPage}/>
       </div>
     );
+  }
+
+  onClickNextPage() {
+    // TODO: create a Model.js file which holds an array of all possible pages, among other things
+    // TODO: use pages array from Model.js to robustly update currentPage
+    this.state.currentPage++;
   }
 }
 
