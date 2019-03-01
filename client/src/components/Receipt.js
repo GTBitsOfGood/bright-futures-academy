e7cfcb8086692a66cbdee5474d99cbe0194cb02b
Michelle Liu <michelleliu343@gmail.com>
2019-03-01 16:02:46 -0500

    Added template data values

--- client/src/components/Receipt.js
+++ client/src/components/Receipt.js
@@ -3,8 +3,6 @@ import logoImage from './../images';
 import ChargesList from './ChargesList';
 import './css/PaymentContent.css';
 
-const NUM_PAGES = 4;
-
 /**
  * Parent class for Payment Page. Contains the text "Account Payment", then the progress bar through the pages, and a
  * placeholder for the current PaymentPageContent.
@@ -15,41 +13,62 @@ class Receipt extends Component {
     super(props);
 
     this.state = {
-        currentPage: 1, // will be used to determine what page user is on. We start at page 1, end on numPages
         activities: [],
+        paymentInfo: {}
     }
   }
 
   componentDidMount() {
     // activities, total, and payment info are placeholders, to be replaced with an API call
     const activities = [{name:"Activity Fee", price:"400"}, {name:"Uniform Fee", price:"40"}, {name:"Transportation Fee", price:"40"}, {name:"Textbook Fee", price:"40"}];
-    this.setState({ activities: activities });
+    const paymentInfo = {
+      billFromName:"Bright Futures Academy", 
+      billFromAddressLine1: "123 Ferst Drive",
+      billFromAddressLine2: "Atlanta, GA 30314",
+      billToName: "User ID", 
+      billToAddressLine1: "321 Techwood Drive",
+      billToAddressLine2: "Buckhead, GA 30305",
+      invoiceNum: 123456789, 
+      invoiceDate: "00/00/00", 
+      total: 0.00,
+      subtotal: 0.00,
+      tax: 0.00
+    }
+    this.setState({ activities: activities, paymentInfo: paymentInfo});
   }
 
   render() {
-    const { activities, currentPage } = this.state;
+    const { activities, paymentInfo } = this.state;
     return (
       <div>
         <h4>Receipt</h4>
-        <img src={logoImage} height="300"/>
+        <img src={logoImage} height="300" alt="Bright Futures Academy Logo"/>
 
         <div>
           <p>Bill From: 
-          <br />Bill To: 
-          <br />Invoice Number #: 
-          <br />Invoice Date: 
-          <br />Amount Due: 
+          <br /> {paymentInfo.billFromName}
+          <br /> {paymentInfo.billFromAddressLine1}
+          <br /> {paymentInfo.billFromAddressLine2} 
+          <br />
+          <br />Bill To:
+          <br /> {paymentInfo.billToName}
+          <br /> {paymentInfo.billToAddressLine1}
+          <br /> {paymentInfo.billToAddressLine2}
+          <br />
+          <br />Invoice Number #: {paymentInfo.invoiceNum}
+          <br />Invoice Date: {paymentInfo.invoiceDate}
+          <br />Amount Due: {"$" + paymentInfo.total}
           </p>
         </div>
        
         <br />
-        <ChargesList activities={this.state.activities}/>
+        <ChargesList activities={activities}/>
         <br />
 
         <div>
-          <p>Subtotal: 
-          <br />Tax: 
-          <br />Total: 
+          <p>Subtotal: {paymentInfo.subtotal}
+          <br />Tax: {paymentInfo.tax}
+          <br />Total: {paymentInfo.total}
           </p>
         </div>  
         
