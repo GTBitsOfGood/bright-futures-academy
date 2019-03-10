fbe28b7c2793c9b5183eb4d880c92ef73b65aad0
Philippe Desrosiers <phildesro@yahoo.com>
2019-03-10 18:50:59 -0400

    Merge conflict

--- server/models/student.js
+++ server/models/student.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const Activity = require('./activity')
-const Schema = mongoose.Schema;
+let mongoose = require('mongoose');
 require('mongoose-type-url');
+
 let Activity = require('./activity')
+
 let Schema = mongoose.Schema;
 
 let StudentSchema = new Schema({
