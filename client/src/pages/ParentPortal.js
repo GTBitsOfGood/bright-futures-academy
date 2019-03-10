61fdc12c39e31316ce5e49df8ec0a5e85b8c84b0
Samyakkumar <samyakkumar@gmail.com>
2019-03-10 16:20:35 -0400

    Rebase to master

--- client/src/pages/ParentPortal.js
+++ client/src/pages/ParentPortal.js
@@ -5,8 +5,12 @@ import AnnouncementList from './../components/Announcements';
 import {Button} from 'reactstrap';
 import './css/ParentPortal.css';
 
-const API_STUDENT_DEV = "http://localhost:3001/api/student/";
+
+//TODO: Figure out he correct URL's
+const API_HOUSEHOLD_DEV = "http://localhost:5000/api/household/";
+const API_STUDENT_DEV = "http://localhost:5000/api/student/"
 const API_STUDENT_PROD = "";
+const API_HOUSEHOLD_PROD = "";
 /**
  * Parent class for Parent portal. Placeholder cards for student information
  */
@@ -17,10 +21,11 @@ class PaymentPortal extends Component {
     
     this.state = {
       announcements: this.props.announcements,
-      students: ""
+      students: [{id: 1234, name : {first: "Name_first", last:"name_last"}, amount_due:2000}]
     }
 
     this.ReactIsInDevelomentMode = this.ReactIsInDevelomentMode.bind(this);
+    this.getStudentInfo()
   }
 
   ReactIsInDevelomentMode(){ 
@@ -28,30 +33,33 @@ class PaymentPortal extends Component {
   }
 
   getStudentInfo(){
-    //TODO: Make an API call to fetch all students of the households
+    //TODO: Figure out how it is being passed later : this.props.houseHoldID
     if (this.ReactIsInDevelomentMode()){
-      let urlToFetch = API_STUDENT_DEV + this.props.houseHoldID;
+      console.log("React is in dev mode...")
+      //Fetch the student list
+      let urlToFetch = API_HOUSEHOLD_DEV + '5c82c92f79971d22f1ce85f0';
       fetch(urlToFetch)
       .then(response => response.json())
-      .then(data => this.parseData())
+      .then(data => this.setState({students: data.students}))
+    } else {
+      console.log("React is in production mode...")
+      let urlToFetch = API_HOUSEHOLD_PROD + '5c82c92f79971d22f1ce85f0';
+      fetch(urlToFetch)
+      .then(response => response.json())
+      .then(data => this.setState({students: data.students}))
     }
   }
-
-  //TODO: ADD KEYS FOR ANNOUNCEMENTS. 
-  //TODO: Add logic for page changing. 
+ 
   //TODO: Add logic for making payment
   render() {
     return (
     <div id='parent-portal-container'>
       <PrimaryNavBar />
       <div className="container"> 
-      <StudentCard studentName={"Name1"} studentID={1} studentBalance={1234}/>
-          <StudentCard studentName={"Name2"} studentID={2} studentBalance={123}/>
-          <StudentCard studentName={"Name2"} studentID={2} studentBalance={123}/>
-          <StudentCard studentName={"Name2"} studentID={2} studentBalance={123}/>
-          <StudentCard studentName={"Name2"} studentID={2} studentBalance={123}/>
-          <StudentCard studentName={"Name2"} studentID={2} studentBalance={123}/>
-      <AnnouncementList announcements={["some stuff1", "some stuff2", "some stuff3"]}/>
+          {this.state.students.map(function(d, id) {
+            return <StudentCard key= {d.id} studentName={d.name.first + " " + d.name.last} studentID = {d.id} studentBalance={d.amount_due} />
+          })}
+      <AnnouncementList announcements={}/>
       </div>
       <Button>Make Payment</Button>
       </div>
