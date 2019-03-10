7022c2fef844113402b1bd6cf6168db483697eb2
Philippe Desrosiers <phildesro@yahoo.com>
2019-03-10 18:50:59 -0400

    Merge conflict

--- server/models/student.js
+++ server/models/student.js
@@ -1,15 +1,8 @@
 let mongoose = require('mongoose');
-<<<<<<< HEAD
 require('mongoose-type-url');
-
-let activity = require('./activity').schema;
-
 
 let Activity = require('./activity')
 
-=======
-let Activity = require('./activity').schema;
->>>>>>> debfb3a3c5f489c84c93d5827699debe7f77a4ce
 let Schema = mongoose.Schema;
 require('mongoose-type-url');
 
@@ -22,16 +15,12 @@ let StudentSchema = new Schema({
         type: Number,
         required: true
     },
-<<<<<<< HEAD
     /**
      * TODO: Fix the type for the activities array
      */
     activities: [
         Object
     ],
-=======
-    activities: [Object],
->>>>>>> debfb3a3c5f489c84c93d5827699debe7f77a4ce
     img_url: {
         type: mongoose.SchemaTypes.Url
     },
