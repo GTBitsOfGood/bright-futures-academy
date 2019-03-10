3d32d71734585711fdd1d71a037f631cf7a7972a
Philippe Desrosiers <phildesro@yahoo.com>
2019-03-10 18:37:42 -0400

    Merge conflicts

--- server/app.js
+++ server/app.js
@@ -2,19 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-const passport = require("passport");
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var householdRouter = require('./routes/householdRouter');
 var studentRouter = require('./routes/studentRouter');
 var activityRouter = require('./routes/activityRouter');
-<<<<<<< HEAD
 var paymentRouter = require('./routes/payment')
-=======
-var schoolInfoRouter = require('./routes/schoolInfoRouter');
->>>>>>> debfb3a3c5f489c84c93d5827699debe7f77a4ce
 
 var app = express();
 
@@ -22,26 +16,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
 
 app.use('/api/household', householdRouter);
 app.use('/api/student', studentRouter);
 app.use('/api/activity', activityRouter);
-app.use('/api/schoolInfo', schoolInfoRouter);
 app.use('/api', indexRouter);
 app.use('/api/payment', paymentRouter)
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(() => {
-  console.log("Connected to MongoDB")
- });
- 
- // Start server to serve endpoints
- console.log('Express started. Listening on port', process.env.PORT || 5000);
- app.listen(process.env.PORT || 5000);
 
 // Render React page (keep this at the bottom of the file)
 app.use(express.static(path.join(__dirname, "../client/build/")));
