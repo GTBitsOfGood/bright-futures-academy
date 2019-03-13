04922d1bb4deae13533ced7bd15b2024e701fc4e
SujeethJinesh <sujeethjinesh@gmail.com>
2019-03-12 20:01:43 -0400

    Add Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import BFSNav from './components/Navbar';
 import PaymentPortal from './pages/ParentPortal';
 import PaymentPage from './pages/PaymentPage';
 import NoMatch from './pages/NoMatch';
+import LandingPage from './pages/LandingPage';
 
 //TODO: Add more pages for routing.
-
 const App = () => (
   <Router>
     <div>
       <Switch>
       <Route path="/makepayment" component={PaymentPortal} />
       <Route path="/paymentPage" component={PaymentPage} />
+      <Route path="/landingPage" component={LandingPage}/>
       <Route component={NoMatch} />
       </Switch>
     </div>
