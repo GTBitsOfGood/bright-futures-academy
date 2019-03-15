46ffb8b7bc0b28eeb35be2bad6de6cf03685382f
Spencer Gold <spencersnotgold@gmail.com>
2019-03-15 17:50:12 -0400

    Remove static methods, avoiding circular dependencies

--- server/models/student.js
+++ server/models/student.js
@@ -30,48 +30,4 @@ let StudentSchema = new Schema({
     }
 })
 
-StudentSchema.statics.getStudents = function(householdId, callback) {
-    Household.findById(householdId, (err, household) => {
-        if (err) {
-            callback(new Error('Could not find household'), {})
-            return
-        }
-        callback({}, household.students)
-    })
-}
-
-StudentSchema.statics.getStudent = function(householdId, studentId, callback) {
-    Household.findById((householdId), (err, household) => {
-        if (err) {
-            callback(new Error('Could not find household'), {})
-            return
-        }
-        const student = household.students.id(studentId)
-        if (student === undefined) {
-            callback(new Error('Could not find student'), {})
-            return
-        }
-        callback({}, student)
-    })
-}
-
-StudentSchema.statics.addStudentToHousehold = function(householdId, student, callback) {
-    console.log(Household)
-    Household.findById((householdId), (err, household) => {
-        if (err) {
-            callback(new Error('Could not find household'), {})
-            return
-        }
-        const student = new
-        household.students.addToSet(student)
-        household.save((err) => {
-            callback('Could not save new student', {})
-            return
-        })
-        callback({}, student)
-    })
-}
-
 module.exports = mongoose.model('Student', StudentSchema);
-
-const Household = require('./household')
