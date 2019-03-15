7ded5e5e5a9ac21fb009c45b15194c382bf0944a
Spencer Gold <spencersnotgold@gmail.com>
2019-03-15 17:50:30 -0400

    Add boilerplate for modular router code

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+export { default as Household } from './household';
+export { default as Student } from './student';
+export { default as Activity } from './activity';
+export { default as SchoolInfo } from './schoolInfo';
