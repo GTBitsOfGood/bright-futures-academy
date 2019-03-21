76cdf30f8decca0989fb26692806118653972488
SujeethJinesh <sujeethjinesh@gmail.com>
2019-03-21 13:52:08 -0400

    Resolve conflicts

--- client/src/components/NewsTile.js
+++ client/src/components/NewsTile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './css/NewsTile.css';
-import logoImage from '../images';
+import { logoImage } from '../images';
 
 /**
  * News tile component that will be used for populating the news landing page component.
@@ -19,7 +19,7 @@ class NewsTile extends Component {
         return (
             <figure>
                 <div className="newsImage">
-                    <img className="newsImage" alt="#" src={logoImage[0]}  width="90%" height="90%"/>
+                    <img className="newsImage" alt="#" src={logoImage}  width="90%" height="90%"/>
                 </div>
                 <figcaption className="newsTileHeader">{header}</figcaption>
                 <figcaption className="newsTileParagraph">{caption}</figcaption>
