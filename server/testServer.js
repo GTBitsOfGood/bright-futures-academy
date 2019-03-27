8595e157dde64dd1e8e3a90ca50ff09ae66911ee
Philippe Desrosiers <phildesro@yahoo.com>
2019-03-26 21:18:35 -0400

    Added auth files

--- server/testServer.js
+++ server/testServer.js
@@ -10,7 +10,7 @@ var activityRouter = require('./routes/activityRouter');
 var schoolInfoRouter = require('./routes/schoolInfoRouter');
 
 require('dotenv').config();
-var app = express();
+//var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
