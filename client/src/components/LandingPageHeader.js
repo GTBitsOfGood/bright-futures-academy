58a5bf817a64f5857c564752d34abdc364d5fc11
Michelle Liu <michelleliu343@gmail.com>
2019-04-05 15:20:33 -0400

    take out the comments whoops

--- client/src/components/LandingPageHeader.js
+++ client/src/components/LandingPageHeader.js
@@ -21,28 +21,6 @@ class LandingPageHeader extends Component {
      * TODO: Add links for social media links
      */
     render() {
-        // return (
-        //         <div className="icons">
-        //             <div className="logo">
-        //                 <img alt="#" src={logoImage} height="100px" />
-        //             </div>
-        //             <div className="phoneNumber">
-        //                 <span className="phoneSpace">
-        //                     <FontAwesomeIcon icon={ faPhone }/>
-        //                     <p className="phoneDigits">
-        //                         404-799-1249
-        //                     </p>
-        //                 </span>
-        //             </div>
-        //             <div className="businessIcons">
-        //                 <a href="https://twitter.com/bfatlorg"><FontAwesomeIcon icon={['fab', 'twitter']}/></a>
-        //                 <a href="https://www.facebook.com/BrightFuturesAtl/"><FontAwesomeIcon icon={['fab', 'facebook-f']}/></a>
-        //                 <a href="https://www.instagram.com/Bfatlorg/"><FontAwesomeIcon icon={['fab', 'instagram']}/></a>
-        //                 <a href="https://www.youtube.com/channel/UCnOZoDqIuwuJFFKkpZ7kn8w"><FontAwesomeIcon icon={['fab', 'youtube']}/></a>
-        //             </div>
-        //         </div>
-        // );
-
         return (
             <div className="icons">
                 <div className="logo">
