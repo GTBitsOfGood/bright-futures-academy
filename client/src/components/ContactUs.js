76693f037c34562efb1e58673b2c5d26970efb93
Michelle Liu <michelleliu343@gmail.com>
2019-04-07 17:56:17 -0400

    Added directions and contact info

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './css/ContactUs.css';
-
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Snackbar } from './';
+import { Directions } from './../components';
 
 /**
  * Contact Us component for contact page.
@@ -68,35 +70,53 @@ class ContactUs extends Component {
 
     render() {
         return (
-            <div className="contactUsContainer">
-                <div className="contactUsMain">
-                    <p className="contactUsHeaderText">
-                        Contact Us
-                    </p>
-                    <p className="contactUsDescriptionText">
-                        Contact us about anything related to our organization, programs and services. Please give us a few details and we’ll get back to you soon.
-                    </p>
-                    <div className="contactUsInputBoxes">
-                        <p className="contactFormText">
-                            Name *
+            <div className="contactUsPageContainer">
+                <div className="contactUsContainer">
+                    <div className="contactUsMain">
+                        <p className="contactUsHeaderText">
+                            Contact Us
                         </p>
-                        <input type="text" name="name" value={this.state.name} onChange={(e) => {this.onValueChange(e)}} placeholder="Insert Name Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
-                        <p className="contactFormText">
-                            Email Address *
+                        <p className="contactUsDescriptionText">
+                            Contact us about anything related to our organization, programs and services. Please give us a few details and we’ll get back to you soon.
                         </p>
-                        <input type="text" name="email" value={this.state.email} onChange={(e) => {this.onValueChange(e)}} placeholder="Insert Email Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
-                        <p className="contactFormText">
-                            Subject *
-                        </p>
-                        <input type="text" name="subject" value={this.state.subject} onChange={(e) => {this.onValueChange(e)}} placeholder="Insert Subject Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
-                        <p className="contactFormText">
-                            Message *
-                        </p>
-                        <textarea type="text" name="message" className="contactUsMessageBox" value={this.state.message} onChange={(e) => {this.onValueChange(e)}} placeholder="Insert Your Message Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
-                        <input id="contactUsSubmitButton" type="button" value="Submit" onClick={this.onSubmit}/>
+                        <div className="contactUsInputBoxes">
+                            <p className="contactFormText">
+                                Name *
+                            </p>
+                            <input type="text" name="name" value={this.state.name} onChange={(e) => {this.onValueChange(e)}} placeholder=" Insert Name Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
+                            <p className="contactFormText">
+                                Email Address *
+                            </p>
+                            <input type="text" name="email" value={this.state.email} onChange={(e) => {this.onValueChange(e)}} placeholder=" Insert Email Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
+                            <p className="contactFormText">
+                                Subject *
+                            </p>
+                            <input type="text" name="subject" value={this.state.subject} onChange={(e) => {this.onValueChange(e)}} placeholder=" Insert Subject Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
+                            <p className="contactFormText">
+                                Message *
+                            </p>
+                            <textarea type="text" name="message" className="contactUsMessageBox" value={this.state.message} onChange={(e) => {this.onValueChange(e)}} placeholder=" Insert Your Message Here" onFocus="if(this.value==this.defaultValue)this.value='';"/>
+                            <input id="contactUsSubmitButton" type="button" value="Submit" onClick={this.onSubmit}/>
+                        </div>
                     </div>
+                    <div className="contactUsInfo">
+                        <div className="contactUsTextContainer">
+                            <FontAwesomeIcon icon={ faPhone } color="#ff4252"/>
+                            <p className="contactUsText">
+                                404-799-1249
+                            </p>
+                        </div>
+                        <div className="contactUsTextContainer">
+                            <FontAwesomeIcon icon={ faEnvelope } color="#ff4252"/>
+                            <p className="contactUsText">
+                                bfinfo@bfatl.org
+                            </p>
+                        </div>
+                    </div>
+                    
                 </div>
                 <Snackbar show={this.state.showSnackbar} message={this.snackbarMessage(this.state.messageSentSuccess)}/>
+                <Directions />
             </div>
         );
     }
