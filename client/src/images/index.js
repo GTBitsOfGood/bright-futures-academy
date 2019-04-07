f6f9bea62172576c88c52baa3e2be533909b4b03
Stephanie <almeidasmail@gmail.com>
2019-04-07 17:04:04 -0400

    add board members tags

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -3,3 +3,4 @@ export { default as infographic } from "./infographic.jpg";
 export { default as gradCap } from "./graduation.png";
 export { default as book } from "./language.png";
 export { default as trophy } from "./trophy.png";
+export { default as founders } from "./founders.jpg";
