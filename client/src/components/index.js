42507be57e756e325fcecaa3f3ebe2314676c33a
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2019-04-09 19:36:20 -0400

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -19,7 +19,6 @@ export { default as Footer } from './Footer';
 export { default as LandingPageNews } from './LandingPageNews';
 export { default as Calendar } from './Calendar';
 export { default as About } from './About';
-export { default as Leadership } from './Leadership';
 export { default as LandingPageTitleCard } from './LandingPageTitleCard';
 export { default as AboutUsVideo } from './AboutUsVideo';
 export { default as ContactUs } from './ContactUs';
