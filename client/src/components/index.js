985366223b0e29662cb6bd564252d277685806b7
Stephanie A <almeidasmail@gmail.com>
2019-04-09 19:47:51 -0400

    About leadership (#199)
    * Change spotlight and impact feature titles and add links
    
    * add board members tags
    
    * Email new password (#175)
    
    * Add email route
    
    * Add Email Route
    
    * Remove package lock
    
    * Fix variable names
    
    * Remove exposed credentials
    
    * Add fix for 404 error in fetch request (#176)
    
    * Add fix for 404 error in fetch request
    
    * Fix redundant code
    
    * Fix syntax error (#183)
    
    * Add landing page title card (#178)
    
    * Start Carousel
    
    * Some progress
    
    * Add Colored Background
    
    * Start Carousel
    
    * Some progress
    
    * Add Colored Background
    
    * Fix landing page route (#180)
    
    * Fix landing page route
    
    * Update Landing Page link
    
    * Contact us page (#186)
    
    * Start Contact Us Page
    
    * Some changes
    
    * Start Contact Us Page
    
    * Some changes
    
    * Add Contact Us Component
    
    * Add Comments
    
    * Phone number visible + icons aligned (#187)
    
    * Phone number visible + icons aligned
    
    * Phone number visible + icons aligned
    
    * take out the comments whoops
    
    * Cleanup 2
    
    * Academics page (#193)
    
    * Template logo, name, title
    
    * Cleanup
    
    * Add fade in description
    
    * Template logo, name, title
    
    * Cleanup
    
    * Add fade in description
    
    * More merge conflicts
    
    * About us video (#179)
    
    * Start About Us Video Component
    
    * Add Video and Side Paragraph
    
    * Start About Us Video Component
    
    * Add Video and Side Paragraph
    
    * Add video text
    
    * Add video text
    
    * Add Scrolling
    
    * Clean up logic
    
    * Start About Us Video Component
    
    * Add Video and Side Paragraph
    
    * Add video text
    
    * Add video text
    
    * Add Scrolling
    
    * Clean up logic
    
    * Resolve Merge Conflicts
    
    * Remove redundant comments
    
    * Start About Us Video Component
    
    * Add Video and Side Paragraph
    
    * Add video text
    
    * Add video text
    
    * Add Scrolling
    
    * Clean up logic
    
    * Start About Us Video Component
    
    * Add Video and Side Paragraph
    
    * Add video text
    
    * Add video text
    
    * Add Scrolling
    
    * Resolve Merge Conflicts
    
    * Remove redundant comments
    
    * Add Comment
    
    * Auth finish (#195)
    
    * Add Auth
    
    * Actually Add Auth
    
    * Add Prod Links
    
    * Add Producation Link
    
    * Delete package-lock.json
    
    * Delete package-lock.json
    
    * Add Comments
    
    * Add hotfix for cors request (#196)
    
    * add board members tags
    
    * Added directions and contact info (#200)
    
    * Added comments
    
    * Update BoardOfDirectors.js
    
    * Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -24,4 +24,5 @@ export { default as AboutUsVideo } from './AboutUsVideo';
 export { default as ContactUs } from './ContactUs';
 export { default as Snackbar } from './Snackbar';
 export { default as Academics } from './Academics';
+export { default as Leadership } from './Leadership';
 export { default as Directions } from './Directions';
